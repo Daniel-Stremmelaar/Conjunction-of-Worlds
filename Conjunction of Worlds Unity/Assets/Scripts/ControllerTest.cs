43b8fa489bd9b8aadb422247465edd8955d10969
Daniël Stremmelaar <97050868@st.deltion.nl>
2020-01-31 16:46:27 +0100

    Main Menu & Controller Test
    Main Menu greybox ready.
    Controller input registration references found, to be tested.

---
+++ Conjunction of Worlds Unity/Assets/Scripts/ControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerTest : MonoBehaviour
+{
+    // square: fire 1
+    // cross: fire 2
+    // circle: fire 3
+    // triangle: jump
+    // horizontal & vertical axis: left stick
+    //info here: https://www.reddit.com/r/Unity3D/comments/1syswe/ps4_controller_map_for_unity/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
